847f800b4e5384e0ee496da1519cf03aef453849
Philipp Meier <pmeier82@googlemail.com>
2013-10-24 15:42:20 +0200

    --x flags for ci scripts

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,7 @@ echo "inside $0"
 
 ## TESTS
 
-echo nosetests --exe -w /tmp -A "$NOSE_ARGS" pandas --show-skipped
-nosetests --exe -w /tmp -A "$NOSE_ARGS" pandas --show-skipped
+nosetests
 
 ## EOF
 true
